d2f046e324960a3eef98b9c3f5a9bc5b1a614f1c
Mitchell Myers <myersmk@umich.edu>
2020-02-07 00:26:50 -0500

    random numbers appear on-screen

--- app/src/main/java/umd/mad/dicegame/GameDie.java
+++ app/src/main/java/umd/mad/dicegame/GameDie.java
@@ -1,20 +1,37 @@
 package umd.mad.dicegame;
 
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
 import java.util.Random;
 
 public class GameDie {
-    private final int STARTING_INDEX = 1; // an added offset to determine where an index starts
+    private final int MIN_DICE_VALUE = 1; // an added offset to determine where an index starts
     private final int MAX_DICE_VALUE = 6;
+    private static int instances = 0;
 
-    private Random r = new Random();
-    private int lastRollResult; // the recorded result since the last roll
+    private final int ID_NUMBER;
+
+    private Random roller = new Random();
+    private Integer lastRollResult; // the recorded result since the last roll
+
+    public GameDie() {
+        instances++;
+        ID_NUMBER = instances;
+    }
 
     public Integer roll() {
-        lastRollResult = r.nextInt(MAX_DICE_VALUE) + STARTING_INDEX; // generate a number between 1 and 6 inclusive
+        lastRollResult = roller.nextInt(MAX_DICE_VALUE) + MIN_DICE_VALUE; // generate a number between 1 and 6 inclusive
         return lastRollResult;
     }
 
-    public int getLastRollResult() {
+    public Integer getLastRollResult() {
         return lastRollResult;
+    }
+
+    public Integer getID_NUMBER() {
+        return ID_NUMBER;
     }
 }
--- app/src/main/java/umd/mad/dicegame/MainActivity.java
+++ app/src/main/java/umd/mad/dicegame/MainActivity.java
@@ -1,20 +1,38 @@
 package umd.mad.dicegame;
 
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Application;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MAX_DICE = 2;
+    private GameDie[] dice = new GameDie[MAX_DICE];
+    private int[] viewIDs = {
+            R.id.textView,
+            R.id.textView2
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void init() {
+        for (int i = 0; i < MAX_DICE; i++) {
+            dice[i] = new GameDie();
+        }
     }
 
     public void quit(View view) {
@@ -24,13 +42,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void play(View view) {
         setContentView(R.layout.activity_main2);
+        init();
     }
 
     public void roll(View view) {
-        GameDie[] dice = new GameDie[2];
+        roll();
+    }
 
-        for (GameDie d : dice) {
-            Log.i("ROLL", d.roll().toString());
+    private void preRoll(int number) {
+        try {
+            for (int i = 0; i < number; i++) {
+                roll();
+                Thread.sleep(200);
+            }
+        }
+        catch (Exception e) {
+            Log.e("EXCEPTION", e.getMessage());
+        }
+    }
+
+    private void roll() {
+        TextView textView;
+
+        for (int i = 0; i < MAX_DICE; i++) {
+            textView = findViewById(viewIDs[i]);
+            textView.setText(dice[i].roll().toString());
         }
     }
 
