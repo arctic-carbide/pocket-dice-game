eab215949afb72560828a7362deb6f46f158868c
Myers <myersmk@UMICH.EDU>
2020-02-13 17:30:28 -0500

    removed trash data from GameDie

--- app/src/main/java/umd/mad/dicegame/GameDie.java
+++ app/src/main/java/umd/mad/dicegame/GameDie.java
@@ -5,9 +5,6 @@ import java.util.Random;
 public class GameDie {
     private final int MIN_DICE_VALUE = 1; // an added offset to determine where an index starts
     private final int MAX_DICE_VALUE = 6;
-    private static int instances = 0;
-
-    private final int ID_NUMBER;
 
     private static final int[] FACES = {
             R.drawable.die_face_1,
@@ -21,11 +18,6 @@ public class GameDie {
     private Random roller = new Random();
     private Integer lastRollResult; // the recorded result since the last roll
 
-    public GameDie() {
-        instances++;
-        ID_NUMBER = instances;
-    }
-
     public Integer roll() {
         lastRollResult = roller.nextInt(MAX_DICE_VALUE) + MIN_DICE_VALUE; // generate a number between 1 and 6 inclusive
         return lastRollResult;
@@ -33,10 +25,6 @@ public class GameDie {
 
     public Integer getLastRollResult() {
         return lastRollResult;
-    }
-
-    public Integer getID_NUMBER() {
-        return ID_NUMBER;
     }
 
     public int getCurrentDieFaceResource() { return FACES[lastRollResult - MIN_DICE_VALUE]; }
